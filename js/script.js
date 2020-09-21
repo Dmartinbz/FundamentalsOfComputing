fd94eea5a7180bc6f4d5ef2448e771ee61ade4c1
Devbz <70300963+Dmartinbz@users.noreply.github.com>
2020-09-21 16:29:51 -0600

    it's time to commit

--- js/script.js
+++ js/script.js
@@ -27,21 +27,3 @@ menu_item.forEach((item) => {
     mobile_menu.classList.toggle("active");
   });
 });
-
-$(".skill-per").each(function () {
-  var $this = $(this);
-  var per = $this.attr("per");
-  $this.css("width", per + "%");
-  $({ animatedValue: 0 }).animate(
-    { animatedValue: per },
-    {
-      duration: 1000,
-      step: function () {
-        $this.attr("per", Math.floor(this.animatedValue) + "%");
-      },
-      complete: function () {
-        $this.attr("per", Math.floor(this.animatedValue) + "%");
-      },
-    }
-  );
-});
