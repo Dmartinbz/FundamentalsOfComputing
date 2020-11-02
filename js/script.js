0202b37ec192734f29fc5c0f4ecc1f9f550b0e1a
Devbz <70300963+Dmartinbz@users.noreply.github.com>
2020-11-02 08:59:29 -0600

    updates10

--- js/script.js
+++ js/script.js
@@ -226,3 +226,16 @@ function showNav(inNavItems){
 	gObj("othercalc").innerHTML = snavHTML;
 	return false;
 }
+
+function myFunction() {
+  var x, text;
+
+  // Get the value of the input field with id="numb"
+  x = document.getElementById("numb").value;
+
+  
+    text = x;
+    unicode = ("&#x" + x)
+  
+  document.getElementById("demo").innerHTML = unicode;
+}
