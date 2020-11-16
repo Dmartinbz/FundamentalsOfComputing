914e06567863cb960d8a0adbb9dff6a8c94c87ff
Devbz <70300963+Dmartinbz@users.noreply.github.com>
2020-11-16 09:45:36 -0600

    update8

--- js/script.js
+++ js/script.js
@@ -31,8 +31,9 @@ menu_item.forEach((item) => {
 function calculateBmi() {
 var weight = document.bmiForm.weight.value
 var height = document.bmiForm.height.value
+
 if(weight > 0 && height > 0){	
-var finalBmi = weight/(height/100*height/100)
+var finalBmi = weight * 703/(height *height)
 document.bmiForm.bmi.value = finalBmi
 if(finalBmi < 18.5){
 document.bmiForm.meaning.value = "That you are too thin."
